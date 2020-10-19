a37786e0cb028a069cb25d0353e033567d4fc7da
Thomas H. Li <34053587+thomas-hai-li@users.noreply.github.com>
2020-10-19 01:05:24 -0400

    add proof of concept

--- src/views/TestMenu.js
+++ src/views/TestMenu.js
@@ -1,41 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import { Dot } from 'react-animated-dots';
 import Cookies from 'js-cookie';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../css/menus.css'
+import io from 'socket.io-client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/menus.css';
+
+const socket = io('http://localhost:4000');
 
 function TestMenu(props) {
+  const [ wantsToReenterUserInfo, setWantsToReenterUserInfo ] = useState(false);
+  const [ patientID, setPatientID ] = useState('');
+  const [ sessionID, setSessionID ] = useState('');
 
-  const [wantsToReenterUserInfo, setWantsToReenterUserInfo] = useState(false);
+  useEffect(() => {
+    registerSession();
+  }, []);
 
   const onBackButtonClick = () => {
     setWantsToReenterUserInfo(true);
-  }
+  };
+
+  // TODO: MOVE THIS TO USERREGISTRATION.js
+  const registerSession = () => {
+    // TODO: socket.id undefined sometimes
+    if (socket.id) {
+      let sessionID = socket.id.toLowerCase().substring(0, 5); // first 5 letters will suffice for session ID
+      setSessionID(sessionID);
+
+      socket.emit(`${userType}Registration`, userFirstName, userLastName);
+
+      socket.on('notify', (examinerID) => {
+        alert(`Examiner (${examinerID}) wants to connect to your session!`);
+      });
+    }
+  };
+
+  const checkPatient = (shortID) => {
+    socket.emit('getPatientWithShortID', shortID, sessionID, (patient) => {
+      console.log(patient);
+    });
+  };
 
   const loggedIn = Cookies.get('loggedIn');
-  const userFirstName = Cookies.get("userFirstName");
-  const userLastName = Cookies.get("userLastName");
-  const userType = Cookies.get("userType");
+  const userFirstName = Cookies.get('userFirstName');
+  const userLastName = Cookies.get('userLastName');
+  const userType = Cookies.get('userType');
 
   if (!loggedIn) {
-    return <Redirect to='/' />
-  }
-  else if (wantsToReenterUserInfo || !userFirstName || !userLastName || !userType) {
-    return <Redirect to={{
-      pathname: '/user-registration',
-      state: { wantsToReenterUserInfo }
-    }} />
+    return <Redirect to="/" />;
+  } else if (wantsToReenterUserInfo || !userFirstName || !userLastName || !userType) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/user-registration',
+          state: { wantsToReenterUserInfo }
+        }}
+      />
+    );
   }
 
   // generate user options
   let userOptions;
-  if (userType === "patient") {
+  if (userType === 'patient') {
     userOptions = (
       <div>
         <p className="m-0">
           Site under construction, stay tuned
-          <span style={{fontSize: '2em'}}>
+          <span style={{ fontSize: '2em' }}>
             <Dot>.</Dot>
             <Dot>.</Dot>
             <Dot>.</Dot>
@@ -49,22 +81,46 @@ function TestMenu(props) {
       <div className="d-flex flex-column">
         <p className="m-0">Select Test Version:</p>
         <Link to="/tests/A">
-          <button className='btn btn-menu btn-outline-primary m-2'>Version A</button>
+          <button className="btn btn-menu btn-outline-primary m-2">Version A</button>
         </Link>
         <Link to="/tests/B">
-          <button className='btn btn-menu btn-outline-primary m-2'>Version B</button>
+          <button className="btn btn-menu btn-outline-primary m-2">Version B</button>
         </Link>
       </div>
     );
   }
 
   return (
-    <div className='TestMenu d-flex flex-column align-items-center'>
+    <div className="TestMenu d-flex flex-column align-items-center">
       <h4>LASTen App</h4>
       <p className="m-0">Welcome, {userFirstName}.</p>
-      <p>You are registered as {userType === "examiner" ? "an" : "a"} {userType}.</p>
+      <p className="m-0">
+        You are registered as {userType === 'examiner' ? 'an' : 'a'} {userType}.
+      </p>
+      <p>
+        Your session ID is <strong>{sessionID}</strong>.
+      </p>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          checkPatient(patientID);
+        }}
+      >
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            autoComplete="off"
+            onChange={(e) => setPatientID(e.target.value)}
+          />
+        </div>
+      </form>
+
       {userOptions}
-      <button className="btn w-100 subtle-label" onClick={onBackButtonClick}>Click here to re-enter your user info.</button>
+      <button className="btn w-100 subtle-label" onClick={onBackButtonClick}>
+        Click here to re-enter your user info.
+      </button>
     </div>
   );
 }
