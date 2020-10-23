a933583132fd409bc7bf1e0144ed759cdf0fbe18
Thomas H Li <thomas.hai.li98@gmail.com>
2020-10-23 00:41:54 -0400

    Updates

---
+++ precache-manifest.5a51e1eb6fbfb134eff82234c8a4052f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2ad7d8dc6b85f09563cfbeb7fa9ef7",
+    "url": "/lasten-app/index.html"
+  },
+  {
+    "revision": "9341936aee1055931ea9",
+    "url": "/lasten-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "67e0d02763d65a0b39a9",
+    "url": "/lasten-app/static/css/main.9185ce52.chunk.css"
+  },
+  {
+    "revision": "9341936aee1055931ea9",
+    "url": "/lasten-app/static/js/2.82f71da2.chunk.js"
+  },
+  {
+    "revision": "3a01609874c3b8a46250e8a81450568e",
+    "url": "/lasten-app/static/js/2.82f71da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e0d02763d65a0b39a9",
+    "url": "/lasten-app/static/js/main.87768cf0.chunk.js"
+  },
+  {
+    "revision": "983dabb47dad26a81c08",
+    "url": "/lasten-app/static/js/runtime-main.158ceb85.js"
+  },
+  {
+    "revision": "532c3830118b16ab1b1a89b571849e85",
+    "url": "/lasten-app/static/media/test-blur.532c3830.jpg"
+  },
+  {
+    "revision": "5ee5746f7443c2ede99b96b14bfaebee",
+    "url": "/lasten-app/static/media/test-blur.5ee5746f.jpg"
+  },
+  {
+    "revision": "65990f4c9d16dd1cc0ca46e9665a68d6",
+    "url": "/lasten-app/static/media/test-butterfly.65990f4c.jpg"
+  },
+  {
+    "revision": "097fa81bfb6824a1fe80a53e62631771",
+    "url": "/lasten-app/static/media/test-croc.097fa81b.jpg"
+  },
+  {
+    "revision": "e3c88a8c046dc2cd6f5bd8502017fde0",
+    "url": "/lasten-app/static/media/test-fork.e3c88a8c.jpg"
+  },
+  {
+    "revision": "26fdbe93cf422d96f0402b98821fff06",
+    "url": "/lasten-app/static/media/test-giraffe.26fdbe93.jpg"
+  },
+  {
+    "revision": "d1bad8943d6a7b5faef97b1d78fcd6db",
+    "url": "/lasten-app/static/media/test-knife.d1bad894.jpg"
+  },
+  {
+    "revision": "7b418cd77d79c1edcb686a93b92a16c5",
+    "url": "/lasten-app/static/media/test-pen.7b418cd7.jpg"
+  },
+  {
+    "revision": "57c2245c7b4aa9b9e9644217c9e0db85",
+    "url": "/lasten-app/static/media/test-pencil.57c2245c.jpg"
+  },
+  {
+    "revision": "1080adcbc79d59db1d0c3336fb13d3c7",
+    "url": "/lasten-app/static/media/test-phone.1080adcb.jpg"
+  },
+  {
+    "revision": "ae8de97ace6df94399c163f875fb3acd",
+    "url": "/lasten-app/static/media/test-pineapple.ae8de97a.jpg"
+  },
+  {
+    "revision": "bee55f9e5426be2dc7fcadf6ed475c23",
+    "url": "/lasten-app/static/media/test-tv.bee55f9e.jpg"
+  },
+  {
+    "revision": "42b74cffda8cc07f276515172f362a16",
+    "url": "/lasten-app/static/media/test.42b74cff.jpg"
+  },
+  {
+    "revision": "43e907905cbaf80f95c0cf13327b5d6e",
+    "url": "/lasten-app/static/media/test.43e90790.jpg"
+  },
+  {
+    "revision": "74e8ac0001f911ba1a437503b648220b",
+    "url": "/lasten-app/static/media/test.74e8ac00.png"
+  },
+  {
+    "revision": "d70945e896e322c5c1df6cae403d8392",
+    "url": "/lasten-app/static/media/test.d70945e8.png"
+  }
+]);
